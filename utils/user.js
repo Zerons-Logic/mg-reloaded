da03e40ecab883980bc3856066ce5ff0ee39bea4
nonnycode <miknonny@gmail.com>
2016-09-05 15:27:07 +0100

    calls generator and sets interval for send

--- utils/user.js
+++ utils/user.js
@@ -1,20 +1,21 @@
 const fs = require('fs')
 const faker = require('faker')
 const mailgun = require('mailgun-js')
+const uuid = require('node-uuid')
 const config = require('./config.json')
 
 const domain = config.domain
 const apiKey = config.apiKey
 const batchSize = config.batchSize
 const mailingList = config.mailingList
+const sendDelay = config.sendDelay
+console.log(config.sendDelay)
+// console.log(mailingList)
 const messageData = config.messageData
 messageData.to = mailingList.address
 
-console.log(messageData)
-
 const mg = mailgun({apiKey, domain})
 
-// string.replace method fixes issues in windows.
 const membersList = fs.readFileSync('mails.txt').toString().replace(/\r\n/g,'\n').split('\n')
 
 const members = membersList.map((mail, i) => {
@@ -28,12 +29,13 @@ const members = membersList.map((mail, i) => {
 
 function* memberSubset () {
   while (members.length > 0) {
-    members.length % batchSize <= 1 ? yield members.splice(0, batchSize) : null
+    members.length % batchSize <= 1 ? yield members.splice(0, batchSize) : yield members.splice(0)
   }
 }
 
 const createMailingList = () => {
   return new Promise((resolve, reject) => {
+    mailingList.address = `${uuid.v4()}@${domain}`
     mg.lists().create(mailingList, (err, body) => {
       if (err) return reject(err)
       return resolve(body)
@@ -41,46 +43,55 @@ const createMailingList = () => {
   })
 }
 
-createMailingList()
-  .then((result) => {
-    console.log(result)
-  })
-  .then(() => {
-    // Add users to mailing list.
-    mg.lists(mailingList.address).members().add({members, subscribed: true}, (err, body) => {
-      if (err) return console.log(err)
-      console.log(body)
+const mgJobID = setInterval(() => {
+  // genrates the batchSize specified by splicing original list
+
+  const mgStatus = memberSubset().next()
+  // checks if members list is empty
+  members.length == 0 ? clearInterval(mgJobID) : null
+  // Create mailing list.
+  createMailingList()
+    .then((result) => {
+      console.log(result)
     })
-  })
-  // .then(() => {
-  //   // Delete mailing List.
-  //   mg.lists(mailingList.address).delete((err, body) => {
-  //     if (err) return console.log(err)
-  //     console.log(body)
-  //   })
-  // })
-  .catch(err => {
-    console.log(err)
-  })
+    // Add users to mailing list.
+    .then(() => {
+      mg.lists(mailingList.address).members()
+      .add({members: mgStatus.value, subscribed: true}, (err, body) => {
+        if (err) return console.log(err)
+        console.log(body)
+      })
+    })
+    // Send message to mailing list.
+    // .then(() => {
+    //   mg.messages().send(messageData, (err, body) => {
+    //     if (err) return console.log(err)
+    //     console.log(body)
+    //   })
+    // })
+    .catch(err => {
+      console.log(err)
+    })
+}, sendDelay)
+
+/**
+ * Call generator with next
+ * set interval
+ * add recipient variable.
+ * work on unsubscribe.
+ * send at a later time in future
+ *
+ */
 
 
 
-// Add members to mailing list.
+ //
+ // const jobID = setInterval(() => {
+ //   const status = memberSubset().next()
+ //   members.length == 0 ? clearInterval(jobID) : null
+ //   console.log(status.value)
+ // }, 3000)
 
-
-
-
-// send message to mailing list.
-
-
-// Delete mailing list.
-// mg.lists(mailingList.address).delete((err, body) => {
-//   if (err) return console.log(err)
-//   console.log(body)
-// })
-
-//
-// console.log(memberSubset().next().value)
 // console.log(memberSubset().next())
 // console.log(memberSubset().next())
 
